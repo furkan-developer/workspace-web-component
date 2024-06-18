c13619d640a79330ee85d7103696377baef01a85
furkan aydin <furknn2112@gmail.com>
2024-06-18 17:50:28 +0300

    ADDED| In-memory database

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Database.efCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("JobDb"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 app.MapGet("/", () => "Hello World!");
 
