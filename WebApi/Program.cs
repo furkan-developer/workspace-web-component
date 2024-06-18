a16e6f584bf08cfaf220d230674aeddc50315dce
furkan aydin <furknn2112@gmail.com>
2024-06-18 17:52:29 +0300

    CREATED| endpoints

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Database.efCore;
+using WebApi.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("JobDb"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+
+app.MapGet("/", ([FromServices] ApplicationDbContext dbContext) =>
+{
+    var jobs = dbContext.Jobs.ToList();
+    return jobs;
+});
+
+app.MapPost("/", ([FromBody] Job job, [FromServices] ApplicationDbContext dbContext) =>
+{
+    dbContext.Jobs.Add(job);
+});
 
 app.Run();
