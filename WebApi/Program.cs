2f1c546e643c1d3365041716168b9ef3d12bd4fe
furkan aydin <furknn2112@gmail.com>
2024-06-18 17:57:58 +0300

    ADDED| Swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,13 +2,34 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Database.efCore;
 using WebApi.Models.Entities;
+using NSwag.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("JobDb"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "JobDb";
+    config.Title = "JobDb v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "JobDb";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 app.MapGet("/", ([FromServices] ApplicationDbContext dbContext) =>
 {
