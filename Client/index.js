b9f88f6d0303d3a555fe8469bb8870d7948b6f99
furkan aydin <furknn2112@gmail.com>
2024-06-19 16:11:31 +0300

    CREATED| dialog element for creating one task

--- Client/index.js
+++ Client/index.js
@@ -1,2 +1,21 @@
 import task from "./components/task.js";
 import stage from "./components/stage.js";
+
+let createTaskDialog = document.getElementById(`create-one-task-dialog`);
+let createTaskBtn = document.getElementById("create-task-button");
+let createTaskDialogCloseBtn = document.getElementById(
+  "create-task-dialog-close-btn"
+);
+let createTaskDialogCreateBtn = document.getElementById(
+  "create-task-dialog-create-button"
+);
+
+createTaskBtn.addEventListener("click", function (e) {
+  createTaskDialog.showModal();
+});
+createTaskDialogCloseBtn.addEventListener("click", () => {
+  createTaskDialog.close();
+});
+createTaskDialogCreateBtn.addEventListener("click", () => {
+    console.log('task will send to server for creating it');
+});
